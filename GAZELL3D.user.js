aea2646f03fdd712ff348adca570bfc14ba7200c
anonymoize <46048137+anonymoize@users.noreply.github.com>
2026-03-20 13:04:59 +0700

    fix overlapping lines for some users

--- GAZELL3D.user.js
+++ GAZELL3D.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         GAZELL3D
 // @namespace    https://github.com/anonymoize/GAZELL3D/
-// @version      1.9.7.2
+// @version      1.9.7.3
 // @description  Reimagine UNIT3D-based torrent pages for readability with a two-column layout, richer metadata presentation, cleaner torrent naming, and minor quality-of-life tweaks.
 // @match        https://aither.cc/torrents/*
 // @match        https://aither.cc/torrents*
@@ -960,7 +960,7 @@
     .gz-dropdown-description br + br {
       display: block;
       content: '';
-      margin-top: -0.5em;
+      margin-top: 0.5em;
     }
 
     .gz-dropdown-description:empty::after {
